8e411dfd968a2e4353c1a917459124deb2621b97
Bruce Omeno <bruceomeno@gmail.com>
2024-03-05 20:15:01 +0300

    Initial commit

---
+++ src/main/java/deploymentpipeline/deploymentpipeline/DeploymentPipelineApplication.java
@@ -0,0 +1,13 @@
+package deploymentpipeline.deploymentpipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeploymentPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeploymentPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/deploymentpipeline/deploymentpipeline/DeploymentPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package deploymentpipeline.deploymentpipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
